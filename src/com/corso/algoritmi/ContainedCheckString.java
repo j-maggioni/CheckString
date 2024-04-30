5f7e40c9b0d26eebb93912a956f15c0d067811a8
j-maggioni <jessicamaggioni99@gmail.com>
2024-04-30 09:41:50 +0200

    fix contained e contains

--- src/com/corso/algoritmi/ContainedCheckString.java
+++ src/com/corso/algoritmi/ContainedCheckString.java
@@ -3,7 +3,9 @@ package com.corso.algoritmi;
 public class ContainedCheckString extends CheckString {
     @Override
     public boolean check (String input, String standard) {
-
+    	input = input.toLowerCase();
+		standard = standard.toLowerCase();
+		
         return  standard.contains(input);
     }
 }
