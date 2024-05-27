32b48e749a72affdb011e777adc1b31664d8d5b0
Farouk elghazi <elghazifarouk112@gmail.com>
2024-05-27 17:29:53 +0200

    Update gioco 1 js + timer e post

--- AutenticazioneWeb/src/main/webapp/resources/js/gioco1.js
+++ AutenticazioneWeb/src/main/webapp/resources/js/gioco1.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", () => {
     let scelta3 = document.getElementById("scelta3");
     let scelta4 = document.getElementById("scelta4");
     let nextQuestionBtn = document.getElementById("nextQuestion");
+    let timerHTML = document.getElementById("timer"); 
+
 
     // variabili
     let arrayPaesi = [];
@@ -14,14 +16,14 @@ document.addEventListener("DOMContentLoaded", () => {
     let arrayDomande = [];
     let domandaAttuale = {};
     let score = 0;
-
+    let timer ;
     // Controllo la risposta
     const checkAnswer = (htmlelement,sceltaText) => {
         let rispostaCorretta = domandaAttuale.rispostaCorretta;
         if (rispostaCorretta === sceltaText) {
             score += 10;
             scoreHTML.textContent = score;
-            htmlelement.backgroundColor = "white";
+            htmlelement.style.backgroundColor = 'lightblue';
         } else {
             alert("Risposta Sbagliata");
           
@@ -30,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     // EventListeners da aggiungere
-    scelta1.addEventListener('click', () => checkAnswer(scelta1.style,scelta1.textContent));
+    scelta1.addEventListener('click', () => checkAnswer(scelta1,scelta1.textContent));
     scelta2.addEventListener('click', () => checkAnswer(scelta2,scelta2.textContent));
     scelta3.addEventListener('click', () => checkAnswer(scelta3,scelta3.textContent));
     scelta4.addEventListener('click', () => checkAnswer(scelta4,scelta4.textContent));
@@ -44,11 +46,12 @@ document.addEventListener("DOMContentLoaded", () => {
                 let paese = {
                     bandiera: element.flags.png,
                     nome: element.name.common,
-                    capitale: element.capital ? element.capital[0] : 'No capital'
+                    capitale: element.capital ? element.capital[0] : 'No capital' // se dovesse servire per gioco 2
                 };
                 arraySoloNomi.push(paese.nome);
                 arrayPaesi.push(paese);
-            });
+            }// } element
+        ); // for each
             return arrayPaesi;
         } catch (error) {
             console.log("Error while fetching data from backend, msg: " + error);
@@ -58,6 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
     // Preparo arrayDomande
     const prepareQuestions = () => {
         let arrayDomandeToReturn = [];
+        // to exclude : "United States Minor Outlying Islands" / "Similar to norway"
         arrayPaesi.forEach((paese) => {
             let questionObj = {};
             let arrayFinal = [];
@@ -107,11 +111,50 @@ document.addEventListener("DOMContentLoaded", () => {
             scelta2.textContent = firstQuestion.answers[1];
             scelta3.textContent = firstQuestion.answers[2];
             scelta4.textContent = firstQuestion.answers[3];
+            startTimer(120, timerHTML);
         }
     };
-
     initializeGame();
 
     // Add event listener to the next question button
     nextQuestionBtn.addEventListener('click',() => getNextQuestion() );
+
+    // Post Score al Backend a fine lavoro
+    const postScore = (score) => {
+        try {
+            fetch("/indovina_bandiera/salvaScore", {
+                method: 'POST', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ score })
+            })
+            .then(response => response.json()) 
+            .then(data => {
+                console.log('Success:', data); 
+                // faccio vedere il popup
+            })
+        } catch (error) {
+            console.error('Error while posting score to the server, msg :', error); 
+        }
+        
+    }
+        // Funzione per avviare il timer
+        const startTimer = (duration, display) => {
+            let timer = duration, minutes, seconds;
+            let interval = setInterval(() => {
+                minutes = parseInt(timer / 60, 10);
+                seconds = parseInt(timer % 60, 10);
+    
+                minutes = minutes < 10 ? "0" + minutes : minutes;
+                seconds = seconds < 10 ? "0" + seconds : seconds;
+    
+                display.textContent = minutes + ":" + seconds;
+    
+                if (--timer < 0) {
+                    clearInterval(interval);
+                    postScore(score); // chiama la funzione postScore quando il timer scade
+                }
+            }, 1000);
+        };
 });
