970e68f76aa7475d585bf85eaeaf932ce64a09a0
Farouk elghazi <elghazifarouk112@gmail.com>
2024-05-29 19:49:57 +0200

    gioco 3 versione 1.0

--- AutenticazioneWeb/src/main/webapp/resources/js/gioco3.js
+++ AutenticazioneWeb/src/main/webapp/resources/js/gioco3.js
@@ -1,123 +1,95 @@
-/*document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
+
     // elementi del DOM da manipolare
-    var nazioneHTML = document.getElementById("nazione");
     let scoreHTML = document.getElementById("score");
-    let capitale1 = document.getElementById("capitale1");
-    let capitale2 = document.getElementById("capitale2");
-    let capitale3 = document.getElementById("capitale3");
-    let capitale4 = document.getElementById("capitale4");
-    let nextQuestionBtn = document.getElementById("nextQuestion");
+    let nazione1HTML = document.getElementById("nazione1");
+    let nazione2HTML = document.getElementById("nazione2");
+    let nazione3HTML = document.getElementById("nazione3");
+    let nazione4HTML = document.getElementById("nazione4");
     let timerHTML = document.getElementById("timer");
+    let bottone_next = document.getElementById("btn_next");
 
     // variabili
-    let arrayCapitali = [];
+    let arrayPaesiInMappa = [];  // code + name 
     let arraySoloNomi = [];
     let arrayDomande = [];
+    let arrayDomandeGiaFatte = [];
     let domandaAttuale = {};
     let score = 0;
     let timer;
 
-    // Controllo la risposta
-    const checkAnswer = (htmlelement, capitaleText) => {
-        let rispostaCorretta = domandaAttuale.rispostaCorretta;
-        if (rispostaCorretta === capitaleText) {
-            score += 10;
-            scoreHTML.textContent = score;
-            htmlelement.style.backgroundColor = 'lightblue';
-        } else {
-            let mesg = document.getElementById("rispostaSbagliata") ;
-            mesg.style.display = "block" ;
-            // Nascondi l'elemento dopo un secondo
-            setTimeout(() => {
-                mesg.style.display = "none";
-            }, 1000); // 1000 millisecondi corrispondono a 1 secondo
-        }
-        getNextQuestion();
-    };
-
-    // EventListeners da aggiungere
-    capitale1.addEventListener('click', () => checkAnswer(capitale1, capitale1.textContent));
-    capitale2.addEventListener('click', () => checkAnswer(capitale2, capitale2.textContent));
-    capitale3.addEventListener('click', () => checkAnswer(capitale3, capitale3.textContent));
-    capitale4.addEventListener('click', () => checkAnswer(capitale4, capitale4.textContent));
-
-    // Recupero dati dal Json
-    const recuperaPaesi = async () => {
-        try {
-            let response = await fetch('./resources/json/all.json');
-            let responseJson = await response.json();
-            responseJson.forEach((element) => {
-                let paese = {
-                    nome: element.name.common,
-                    capitale: element.capital ? element.capital[0] : 'No capital'
-                };
-                arraySoloNomi.push(paese.capitale);
-                arrayCapitali.push(paese);
-            });
-            return arrayCapitali;
-        } catch (error) {
-            console.log("Error while fetching data from backend, msg: " + error);
-        }
-    };
+    // extract from html --> array of countries
+    const extractPaesiFromHtml = () => {
+        const paths = document.querySelectorAll('.ag-canvas_svg path');
+        paths.forEach((path) => {
+            let nazione = {};
+            const code = path.getAttribute('id').toString();
+            const nome = path.getAttribute('title').toString();
+            nazione.code = code;
+            nazione.nome = nome;
+            arraySoloNomi.push(nome);
+            arrayPaesiInMappa.push(nazione);
+        });
+    }
+    extractPaesiFromHtml();
 
     // Preparo arrayDomande
     const prepareQuestions = () => {
         let arrayDomandeToReturn = [];
-        arrayCapitali.forEach((paese) => {
+        arrayPaesiInMappa.forEach(nazione => {
             let questionObj = {};
             let arrayFinal = [];
-            let arrayCapitaliDiverse = arraySoloNomi.filter(capitale => capitale !== paese.capitale);
-            let arra3Anwsers = [...arrayCapitaliDiverse].sort(() => Math.random() - 0.5).slice(0, 3);
-            arrayFinal.push(paese.capitale, arra3Anwsers[0], arra3Anwsers[1], arra3Anwsers[2]);
-            questionObj.nazione = paese.nome;
-            questionObj.rispostaCorretta = paese.capitale;
+            let arrayPaesiSbagliati = arraySoloNomi.filter(nome => nome !== nazione.nome);
+            let arra3Anwsers = [...arrayPaesiSbagliati].sort(() => Math.random() - 0.5).slice(0, 3);
+            arrayFinal.push(nazione.nome, arra3Anwsers[0], arra3Anwsers[1], arra3Anwsers[2]);
+            questionObj.paeseDaFillare = nazione.code; // id dell path
+            questionObj.rispostaCorretta = nazione.nome; // tag Title
             questionObj.answers = [...arrayFinal].sort(() => Math.random() - 0.5);
             arrayDomandeToReturn.push(questionObj);
             arrayDomande.push(questionObj);
         });
         return arrayDomandeToReturn;
     };
+    prepareQuestions();
 
-    // Metodo per prendere la domanda successiva
-    const getNextQuestion = async (capitale) => {
+    const getNextQuestion = () => {
         if (arrayDomande.length === 0) {
             arrayDomande = prepareQuestions();
         }
-
-        let DomandeEsclusaPreced = arrayDomande.filter(c => c.nazione !== capitale);
-        let randomIndex = Math.floor(Math.random() * DomandeEsclusaPreced.length);
-        let DomandaNonRipetuta = DomandeEsclusaPreced[randomIndex];
-        domandaAttuale = DomandaNonRipetuta;
-
-        nazioneHTML.innerHTML = DomandaNonRipetuta.nazione;
-        capitale1.textContent = DomandaNonRipetuta.answers[0];
-        capitale2.textContent = DomandaNonRipetuta.answers[1];
-        capitale3.textContent = DomandaNonRipetuta.answers[2];
-        capitale4.textContent = DomandaNonRipetuta.answers[3];
-
+        document.getElementById(domandaAttuale.paeseDaFillare).style.fill = "#59616e"; // #383d46  F00
+        let arrayX = arrayDomande.filter(domanda => {
+            return !arrayDomandeGiaFatte.some(domandaFatta => domanda.paeseDaFillare === domandaFatta.paeseDaFillare);
+        });
+        if (arrayX.length === 0) return;
+        let randomIndex = Math.floor(Math.random() * arrayX.length);
+        let domandaDaFare = arrayX[randomIndex];
+        document.getElementById(domandaDaFare.paeseDaFillare).style.fill = "#F00"; // #383d46  F00
+        nazione1HTML.textContent = domandaDaFare.answers[0];
+        nazione2HTML.textContent = domandaDaFare.answers[1];
+        nazione3HTML.textContent = domandaDaFare.answers[2];
+        nazione4HTML.textContent = domandaDaFare.answers[3];
+        domandaAttuale = domandaDaFare;
+        arrayDomandeGiaFatte.push(domandaDaFare);
     };
 
     // Prima pagina del gioco
     const initializeGame = async () => {
-        arrayPaesi = await recuperaPaesi();
-        arrayDomande = await prepareQuestions();
         if (arrayDomande.length > 0) {
             let randomIndex = Math.floor(Math.random() * arrayDomande.length);
             let firstQuestion = arrayDomande[randomIndex];
+            document.getElementById(firstQuestion.paeseDaFillare).style.fill = "#F00"; // #383d46  F00
+            nazione1HTML.textContent = firstQuestion.answers[0];
+            nazione2HTML.textContent = firstQuestion.answers[1];
+            nazione3HTML.textContent = firstQuestion.answers[2];
+            nazione4HTML.textContent = firstQuestion.answers[3];
             domandaAttuale = firstQuestion;
-            nazioneHTML.innerHTML = firstQuestion.nazione;
-            capitale1.textContent = firstQuestion.answers[0];
-            capitale2.textContent = firstQuestion.answers[1];
-            capitale3.textContent = firstQuestion.answers[2];
-            capitale4.textContent = firstQuestion.answers[3];
-            startTimer(120, timerHTML);
+            arrayDomandeGiaFatte.push(firstQuestion);
+            startTimer(5, timerHTML);
         }
     };
-    initializeGame();
 
     // Add event listener to the next question button
-    nextQuestionBtn.addEventListener('click',() => getNextQuestion() );
-
+    bottone_next.addEventListener('click', getNextQuestion);
     // Funzione per avviare il timer
     const startTimer = (duration, display) => {
         let timer = duration, minutes, seconds;
@@ -126,26 +98,41 @@
             seconds = parseInt(timer % 60, 10);
             minutes = minutes < 10 ? "0" + minutes : minutes;
             seconds = seconds < 10 ? "0" + seconds : seconds;
-
             display.textContent = minutes + ":" + seconds;
 
             if (--timer < 0) { // quando scade il timer
                 clearInterval(interval);
-                let modal = document.getElementById("gameOverModal_gioco3")
-                modal.style.display = "contents"
-                document.getElementById("contenutoGioco").style.display = "none"
-
+                document.getElementById("contenutoGioco").classList.add('hidden'); 
+                document.getElementById("nextQuestion").classList.add('hidden');
+                let modal = document.getElementById("gameOverModal_gioco3");
+                modal.style.display = "contents";
                 var scoreField = document.getElementById("punti");
                 scoreField.value = score;
             }
         }, 1000);
     };
-});*/
 
-document.getElementById('viewLeaderboardButton_gioco3').addEventListener('click', function(event) {
-    event.preventDefault();
+    initializeGame();
 
-    var form = document.getElementById("giocoVO_gioco3");
-    form.submit();
-
+    // Controllo la risposta
+    const checkAnswer = (sceltaText) => {
+        let rispostaCorretta = domandaAttuale.rispostaCorretta;
+        if (rispostaCorretta === sceltaText) {
+            score += 10;
+            scoreHTML.textContent = score;
+        } else { // se la risposta è sbagliata
+            let messaggioErrore = document.getElementById("rispostaSbagliata");
+            messaggioErrore.style.display = "block";
+            // Nascondi l'elemento dopo un secondo
+            setTimeout(() => {
+                messaggioErrore.style.display = "none";
+            }, 1000);
+        }
+        getNextQuestion();
+    };
+    // EventListeners da aggiungere
+    nazione1HTML.addEventListener('click', () => checkAnswer(nazione1HTML.textContent));
+    nazione2HTML.addEventListener('click', () => checkAnswer(nazione2HTML.textContent));
+    nazione3HTML.addEventListener('click', () => checkAnswer(nazione3HTML.textContent));
+    nazione4HTML.addEventListener('click', () => checkAnswer(nazione4HTML.textContent));
 });
